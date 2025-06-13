aa475de9d787d503ea5c9c07f6fc1a0e356fd03d
Daniel King <daniel.king@itpie.com>
2025-06-13 18:18:57 -0400

    Add static files, enhance deploy config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,22 +1,33 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	// Use slog.New() to initialize a new logger with some additional configuration.
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
 	// Use the http.NewServeMux() function to initialize a new servemux, then register the home function as the handler for the "/" URL path.
 	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
 	mux.HandleFunc("GET /{$}", home) // The {$} prevents the home function from being called for any URL path that starts with "/".
 	mux.HandleFunc("GET /character/view/{id}", characterView)
 	mux.HandleFunc("GET /character/create", characterCreate)
 	mux.HandleFunc("POST /character/create", characterCreatePost)
 
 	// Print a log message indicating that the server is starting on port 4000.
-	log.Print("Starting server on port 4000")
+	logger.Info("Starting server", "addr", *addr)
 
 	// Use the http.ListenAndServe() function to start a new web server.
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	err := http.ListenAndServe(*addr, mux)
+	logger.Error(err.Error())
+	os.Exit(1)
 }
